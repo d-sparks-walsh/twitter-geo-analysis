2a4b0ce9e4ecb3bf71203dd316ca3bb369361fbe
d-sparks-walsh <denniswalsh05@gmail.com>
2016-08-11 17:15:17 -0700

    Update SentimentAnalysis.r

--- SentimentAnalysis.r
+++ SentimentAnalysis.r
@@ -4,10 +4,10 @@ library(RDSTK)
 library(httr)
 
 # Stores access tokens from dev.twitter.com
-api_key = "ThdyCndDxrLewFHvuYIV4Xb49"
-api_secret ="L9QPgXrB0H3pPdXJwzH7vU38o3wfMRXlSi8utvBWYKuHoHZWod"
-access_token = "235832591-DrDjV1M7poSxvBh9AWTCZHDWikKRFnuxtRyvBHnt"
-access_token_secret = "woMMg3aIqU8FAkPbkkMZpXSE1uUJYIWfOrN5aBQiE3cfA"
+api_key = ""
+api_secret =""
+access_token = ""
+access_token_secret = ""
 # Authenticate
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
